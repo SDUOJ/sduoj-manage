0d0976187b9b4a2ca80d10204af449a7dcf3b166
jesHrz <jeshrz063@gmail.com>
2020-10-25 10:59:52 +0800

    add two different type of Code Editor

---
+++ src/components/editor/JsonEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <codemirror :value="code" :options="cmOptions" @input="onEditorCodeChange" />
+</template>
+
+<script>
+import { codemirror } from 'vue-codemirror-lite';
+// closebrackets
+import 'codemirror/addon/edit/closebrackets.js'
+
+export default {
+  components: {
+    codemirror
+  },
+  props: {
+    code: {
+      type: String,
+      default: ''
+    }
+  },
+  data: function() {
+    return {
+      cmOptions: {
+        autoCloseBrackets: true,
+        tabSize: 2,
+        line: true,
+        theme: 'default',
+        mode: 'text/json'
+      }
+    }
+  },
+  methods: {
+    onEditorCodeChange: function(newCode) {
+      this.$emit('update:code', newCode)
+    }
+  }
+}
+</script>
