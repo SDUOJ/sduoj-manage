49c9f532d0004008a870ca4677bf0ad60d170832
zhangt2333 <zhangt2333@gmail.com>
2020-11-24 23:09:29 +0800

    update license (#25)
    * license
    
    * license

--- src/components/editor/CodeEditor.vue
+++ src/components/editor/CodeEditor.vue
@@ -1,3 +1,13 @@
+<!--
+   Copyright 2020-2020 the original author or authors.
+
+   Licensed under the General Public License, Version 3.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+        https://www.gnu.org/licenses/gpl-3.0.en.html
+ -->
+
 <template>
   <div style="line-height: normal; font-size: 16px">
     <codemirror :value="code" :options="cmOptions" @input="onEditorCodeChange" />
--- src/components/upload/UploadExcel.vue
+++ src/components/upload/UploadExcel.vue
@@ -1,3 +1,13 @@
+<!--
+   Copyright 2020-2020 the original author or authors.
+
+   Licensed under the General Public License, Version 3.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+        https://www.gnu.org/licenses/gpl-3.0.en.html
+ -->
+
 <template>
   <div class="ivu-upload">
     <div class="ivu-upload ivu-upload-drag" @click="handleUpload" @drop="handleDrop" @dragover="handleDragover"
